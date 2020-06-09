9bbd00da3d0ea5c9791aeaca0a34ae39bc4360c5
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-09 12:11:56 +0100

    department
    written code for test and department

--- src/main/java/com/example/codeclan/Relationships/models/Department.java
+++ src/main/java/com/example/codeclan/Relationships/models/Department.java
@@ -1,4 +1,45 @@
 package com.example.codeclan.Relationships.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name")
+    private String name;
+
+    @JsonIgnoreProperties("department")
+
+    @OneToMany(mappedBy="department")
+    private List<Employee> employees;
+
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
--- src/test/java/com/example/codeclan/Relationships/RelationshipsApplicationTests.java
+++ src/test/java/com/example/codeclan/Relationships/RelationshipsApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.codeclan.Relationships;
 
+import com.example.codeclan.Relationships.models.Department;
+import com.example.codeclan.Relationships.models.Employee;
+import com.example.codeclan.Relationships.models.Project;
+import com.example.codeclan.Relationships.repositories.DepartmentRepository;
+import com.example.codeclan.Relationships.repositories.EmployeeRepository;
+import com.example.codeclan.Relationships.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RelationshipsApplicationTests {
 
+
+	@Autowired private EmployeeRepository employeeRepository;
+	@Autowired private DepartmentRepository departmentRepository;
+	@Autowired private ProjectRepository projectRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canCreateAndSaveAEmployeeProjectDepartment(){
+		Department department = new department("justAName");
+		Department.save(department);
+
+		Project project = new Project("ehhh",69);
+		Project.save(project);
+
+		Employee employee = new Employee("Jim","Jarvis",666,department);
+		employee.addProject(project);
+
 	}
 
 }
