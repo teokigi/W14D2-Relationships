ec70f406449dfe655c9c0b59ea79c0c6a2b83032
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-09 14:37:35 +0100

    Update RelationshipsController.java
    added controller to be able to fecth 1 record of each table

--- src/main/java/com/example/codeclan/Relationships/controllers/RelationshipsController.java
+++ src/main/java/com/example/codeclan/Relationships/controllers/RelationshipsController.java
@@ -8,9 +8,11 @@ import com.example.codeclan.Relationships.repositories.EmployeeRepository;
 import com.example.codeclan.Relationships.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RelationshipsController {
@@ -21,8 +23,23 @@ public class RelationshipsController {
 
     @GetMapping(value = "/employees")
     public List<Employee> getAllEmployees(){ return employeeRepository.findAll();}
+    @GetMapping(value = "/employees/{id}")
+    public Optional<Employee> getEmployee(@PathVariable Long id){
+        return employeeRepository.findById(id);
+    }
+
     @GetMapping(value = "/departments")
     public List<Department> getAllDepartments(){ return departmentRepository.findAll();}
+    @GetMapping(value = "/departments/{id}")
+    public Optional<Department> getDepartment(@PathVariable Long id){
+        return departmentRepository.findById(id);
+    }
+
     @GetMapping(value = "/projects")
     public List<Project> getAllProjects(){ return projectRepository.findAll();}
+    @GetMapping(value = "/projects/{id}")
+    public Optional<Project> getProject(@PathVariable Long id){
+        return projectRepository.findById(id);
+    }
+
 }
